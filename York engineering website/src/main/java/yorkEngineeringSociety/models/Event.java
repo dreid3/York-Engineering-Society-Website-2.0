c58e5eace0a804dde830ca3358ef966e8b502623
RcVincent <rvincent@ycp.edu>
2017-10-20 13:24:15 -0400

    added a very rough outline and functionality to the event class

--- York engineering website/src/main/java/yorkEngineeringSociety/models/Event.java
+++ York engineering website/src/main/java/yorkEngineeringSociety/models/Event.java
@@ -1,14 +1,31 @@
 package yorkEngineeringSociety.models;
 
-
+//import java.util.Date; 
 import java.util.Calendar;
 import java.text.DateFormat; 
 
 public class Event {
 	private String Address; 
-	private Calendar calendar; 
+	//Date eventdate; 
+	//calendar instance for the event 
+	Calendar calen = Calendar.getInstance(); 
 	
 	public Event() {
 		
+	}
+	
+	public void createEvent(String address, int day, int month, int year) {
+		this.setAddress(address);
+		day = calen.get(Calendar.DAY_OF_MONTH);
+		month = calen.get(Calendar.MONTH);
+		year = calen.get(Calendar.YEAR); 
+	}
+
+	public String getAddress() {
+		return Address;
+	}
+
+	public void setAddress(String address) {
+		Address = address;
 	}
 }
